189e9017eee5dd1e611414ccb77b18b5100fde57
jyPark <qkrwnddus93@jbnu.ac.kr>
2023-11-10 23:53:51 +0900

    calc.py의 기능들 main_test에서 구동가능한지 확인
    계산을 위한 로직 일부 구현중

--- calculator/calc.py
+++ calculator/calc.py
@@ -21,3 +21,35 @@ def multi(a,b):
   except:
     print(f"곱하기 에러 발생")
     return None
+#
+# def Operands(infix):
+#     numbers= list('01234569.')
+#     recognized = []
+#
+#     i = 0
+#     while i < len(infix):
+#        j = 1
+#        if infix[i] in numbers:
+#          while i + j < len(infix):
+#            if infix[i+j] in numbers:
+#              j += 1
+#            else:
+#              break
+#          recognized.append(''.join(infix[i:i+j]))
+#          i += j
+#        else:
+#          recognized.append(infix[i])
+#          i += 1
+#     return recognized
+#
+# # def InToPost(infix)
+# #    s = Stack()
+# #    postfix = []
+# #    for i in infix:
+# #      if i == '(':
+# #        s.push(i)
+# #      else if i == ')':
+# #        while s.top() != '(':
+# #          postfix.append(s.pop())
+# #        s.pop()
+# #      else if in priority:
--- calculator/test.py
+++ calculator/test.py
@@ -1,1 +1,13 @@
+import calc
+
 print("연동 테스트파일입니다.")
+
+print("입력 예 : 2x6,2+4,(2+4)*2")
+a = int(input())
+b = int(input())
+c = calc.plus(a,b)
+d = calc.minus(a,b)
+f = calc.multi(a,b)
+print(c,d,f)
+
+
