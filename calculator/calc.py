5a6ed956344ebe3af52bcb0871d8b2ec7643b081
jyPark <qkrwnddus93@jbnu.ac.kr>
2023-11-11 20:59:37 +0900

    calc.py의 기능들 main_test에서 구동가능한지 확인
    계산을 위한 로직 일부 구현중

--- calculator/calc.py
+++ calculator/calc.py
@@ -1,3 +1,8 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from stack import Stack
+
 def plus(a,b):
   try:
     result = a + b
@@ -22,34 +27,46 @@ def multi(a,b):
     print(f"곱하기 에러 발생")
     return None
 #
-# def Operands(infix):
-#     numbers= list('01234569.')
-#     recognized = []
+def Operands(infix):
+  numbers = list('0123456789.')
+  recognized = []
+
+  i = 0
+  while i < len(infix):
+    j = 1
+    if infix[i] in numbers:
+      while i + j < len(infix):
+        if infix[i + j] in numbers:
+          j += 1
+        else:
+          break
+      recognized.append(''.join(infix[i:i + j]))
+      i += j
+    else:
+      recognized.append(infix[i])
+      i += 1
+  return recognized
+
+# def InToPost(infix):
+#   s = stack.Stack()
+#   postfix = []
+#   for i in infix:
+#     if i == '(':
+#       s.push(i)
+#     elif i == ')':
+#       while s.top() != '(':
+#         postfix.append(s.pop())
+#       s.pop()
+#     elif i in priority:
+#       while not s.isEmpty():
+#         if priority[s.top()] >= priority[i]:
+#           postfix.append(s.pop())
+#         else:
+#           break
+#       s.push(i)
+#     else:
+#       postfix.append(i)
+#   while not s.isEmpty():
+#     postfix.append(s.pop())
 #
-#     i = 0
-#     while i < len(infix):
-#        j = 1
-#        if infix[i] in numbers:
-#          while i + j < len(infix):
-#            if infix[i+j] in numbers:
-#              j += 1
-#            else:
-#              break
-#          recognized.append(''.join(infix[i:i+j]))
-#          i += j
-#        else:
-#          recognized.append(infix[i])
-#          i += 1
-#     return recognized
-#
-# # def InToPost(infix)
-# #    s = Stack()
-# #    postfix = []
-# #    for i in infix:
-# #      if i == '(':
-# #        s.push(i)
-# #      else if i == ')':
-# #        while s.top() != '(':
-# #          postfix.append(s.pop())
-# #        s.pop()
-# #      else if in priority:##
+#   return postfix
